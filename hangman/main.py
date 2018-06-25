3c19e11743cfb521a0cc21c2aed17f1603581f6b
Unknown <jason.millsom@gmx.net>
2018-06-25 11:30:27 +0200

    Update Added Welcome message
    Import game is commented out

---
+++ hangman/main.py
@@ -0,0 +1,19 @@
+# import game
+# import player
+
+print("Welcome to Hangman")
+print("Prepare to get hanged")
+print("   ____")
+print("  |    |")
+print("  |    o")
+print("  |   /|\ ")
+print("  |    |")
+print("  |   / \ ")
+print(" _|_")
+print("|   |______")
+print("|          |")
+print("|__________|")
+print("")
+print("")
+print("")
+print("Type in a very flamboyant name: ")
